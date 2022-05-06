253b15e6b498b394b80a3e1f5e82799ca21fe1f5
Dave Lilleystone <davelilleystone@gmail.com>
2022-05-06 14:03:53 +0100

    return to basic component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
 import React from "react";
 
 const App = () => {
-  const title = "My Blog Post";
-  const body = "blog body";
-  const comment = [
-    { id: 1, comment: "comment 1" },
-    { id: 2, comment: "comment 2" },
-    { id: 3, comment: "comment 3" },
-  ];
-
-  const loading = false;
-  const showComments = true;
-  const commentBlock = (
-    <ul>
-      {comment.map((comment) => {
-        return <li key={comment.id}>{`${comment.id} : ${comment.comment}`}</li>;
-      })}
-    </ul>
-  );
-
-  if (loading) return <h1>Loading...</h1>;
-
   return (
-    <>
-      <div className="container">
-        <h1>{title}</h1>
-        <p>{body}</p>
-        {showComments && commentBlock}
-      </div>
-    </>
+    <div className="container">
+      <h1>My App</h1>
+    </div>
   );
 };
 
